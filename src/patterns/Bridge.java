6f8d118b02c421b0bfba52781745d7f46a5b4c3b
testoccgg <testoccgg@gmail.com>
2023-03-07 22:41:53 +0300

    added Bridge pattern

---
+++ src/patterns/Bridge.java
@@ -0,0 +1,36 @@
+package patterns;
+
+public interface Bridge {}
+
+abstract class Computer{
+    OS currentOS;
+    void startPC() {
+        currentOS.prepareFileSystem();
+        currentOS.boot();
+    }
+    void resetPC() {}
+
+    class Macbook extends Computer{}
+    class Dellbook extends Computer{}
+    class Sonybook extends Computer{}
+}
+
+abstract class OS{
+    abstract void boot();
+    abstract void prepareFileSystem();
+
+    class Windows extends OS{
+        @Override
+        void boot() {new String("BIOS");}
+        @Override
+        void prepareFileSystem() {new String("GPT");}
+    }
+    class MacOS extends OS{
+        @Override
+        void boot() {new String("EFI");}
+        @Override
+        void prepareFileSystem() {new String("MBR");}
+    }
+}
+
+
